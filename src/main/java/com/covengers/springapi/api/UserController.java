12a19f3e01ede88145ec648215e693d0ee9c4db1
dntjd7701 <dntjd7701@gmail.com>
2021-08-19 18:35:10 +0900

    #Dr.[ADD]:SMS API

--- src/main/java/com/covengers/springapi/api/UserController.java
+++ src/main/java/com/covengers/springapi/api/UserController.java
@@ -1,7 +1,9 @@
 package com.covengers.springapi.api;
 
 
+import com.covengers.springapi.model.Sms;
 import com.covengers.springapi.model.User;
+import com.covengers.springapi.repo.UserRepository;
 import com.covengers.springapi.service.SmsService;
 import com.covengers.springapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private UserService userService;
     @Autowired
@@ -27,12 +32,23 @@ public class UserController {
     }
 
     @ResponseBody
-    @PostMapping(value = "/sendSms")
-    public ResponseEntity<?> sendSms(HttpServletRequest request) throws Exception {
-        Boolean result = smsService.sms(request);
+    @PostMapping(value = "/api/sendSms")
+    public ResponseEntity<?> sendSms(@RequestBody Sms sms) throws Exception {
+        Boolean result = smsService.sms(sms);
         if (result) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>("fail",HttpStatus.EXPECTATION_FAILED);
+    }
+
+//---------------------------------------------------
+
+    @PostMapping("/api/login")
+    public ResponseEntity<?> Login(@RequestBody User user){
+        System.out.println(user);
+        User user2 = new User();
+        user2 = userRepository.findByUsername(user.getUsername());
+        System.out.println(user2);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/covengers/springapi/service/SmsService.java
+++ src/main/java/com/covengers/springapi/service/SmsService.java
@@ -1,36 +1,37 @@
 package com.covengers.springapi.service;
 
+import com.covengers.springapi.model.Sms;
 import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
 @Service
 public class SmsService{
     private static String CODENUMBER = "-9999";
 
-    public Boolean sms(HttpServletRequest request) throws Exception {
+    public Boolean sms(Sms sms) throws Exception {
+
         String api_key = "NCS24FKUXYQBKKP5";
         String api_secret = "OPTNWRSSEW1D8ANZMB2DN6QICTIF9BMG";
 
         Message coolsms = new Message(api_key, api_secret);
 
         HashMap<String, String> params = new HashMap<String, String>();
-        params.put("to", (String)request.getParameter("to"));
+        params.put("to", sms.getRecipients());
         params.put("from", "01025271985");
         params.put("type", "SMS");
-        params.put("text", (String)request.getParameter("text"));
+        params.put("text", sms.getText());
         params.put("app_version", "test app 1.2"); // application name and version
 
         System.out.println(params);
         try {
-            CODENUMBER = (String)request.getParameter("text");
+            CODENUMBER = sms.getText();
             System.out.println("인증번호는 " + CODENUMBER +" 입니다.");
             JSONObject obj = (JSONObject) coolsms.send(params);
-            System.out.println(obj.toString());
+            System.out.println(obj.toString() + ":::::");
 
         } catch (CoolsmsException e) {
             System.out.println(e.getMessage());
