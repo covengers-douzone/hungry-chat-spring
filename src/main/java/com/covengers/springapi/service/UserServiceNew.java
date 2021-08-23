df7dc626478243db378095fe3c465f7587b44121
JaeHyeon Son <sjh9391985@gmail.com>
2021-08-23 14:03:35 +0900

    #Hulk:[ADD]: 수정

--- src/main/java/com/covengers/springapi/service/UserServiceNew.java
+++ src/main/java/com/covengers/springapi/service/UserServiceNew.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserServiceNew {
    User saveUser(User user);
-//   void addRoleToUser(String username, String role);
+   //   void addRoleToUser(String username, String role);
    User getUser(String username);
    List<User> getUsers(); // 전체 유저 가져오기
 }
