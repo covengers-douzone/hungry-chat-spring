237c19fee2aaa32f5d34316cb07583e6b311019d
dntjd7701 <dntjd7701@gmail.com>
2021-09-15 12:35:50 +0900

    #Dr.[ADD]:Swagger 추가

--- src/main/java/com/covengers/springapi/api/UserAuthController.java
+++ src/main/java/com/covengers/springapi/api/UserAuthController.java
@@ -3,6 +3,7 @@ package com.covengers.springapi.api;
 import com.covengers.springapi.dto.JsonResult;
 import com.covengers.springapi.model.User;
 import com.covengers.springapi.service.UserServiceNew;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserAuthController {
     private final UserServiceNew userServiceNew;
 
-
+    @ApiOperation(value = "Settings로부터의 회원 비밀번호 변경")
     @PostMapping("/passwordupdate")
     ResponseEntity<?> userPasswordUpdate(@RequestBody User data) throws Exception {
 
--- src/main/java/com/covengers/springapi/api/UserControllerNew.java
+++ src/main/java/com/covengers/springapi/api/UserControllerNew.java
@@ -5,6 +5,7 @@ import com.covengers.springapi.dto.Sms;
 import com.covengers.springapi.model.User;
 import com.covengers.springapi.service.SmsService;
 import com.covengers.springapi.service.UserServiceNew;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +20,13 @@ public class UserControllerNew {
     private final UserServiceNew userServiceNew;
     private final SmsService smsService;
 
+    @ApiOperation(value = "get memebers for test", tags = "test")
     @GetMapping("/users")
     ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userServiceNew.getUsers());
     }
 
+    @ApiOperation(value = "휴면 계정 아이디/패스워드 확인 및 결과 응답")
     @PostMapping("/activation")
     public ResponseEntity<?> activation(@RequestBody User user){
         User userinfo = userServiceNew.getUserAndPassword(user.getUsername(), user.getPassword());
@@ -33,6 +36,7 @@ public class UserControllerNew {
              return ResponseEntity.ok().body(new JsonResult(userinfo.getIsDeleted()));
     }
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody User user){
         // 200 ok 보다는 201 created 가 좀 더 정확한 응답. 아래와 같이 바꾼다. created 는 uri 주소가 필요함
@@ -52,6 +56,8 @@ public class UserControllerNew {
         return ResponseEntity.badRequest().body(new JsonResult("이미 존재하는 아이디입니다.", 400));
     }
 
+
+    @ApiOperation(value = "핸드폰 인증을 위한 코드 발송 및 검증")
     @PostMapping(value = "/sms")
     public ResponseEntity<?> sendSms(@RequestBody Sms sms) throws Exception {
         System.out.println(sms.getRecipients() + ":" + sms.getText());
@@ -63,6 +69,7 @@ public class UserControllerNew {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new JsonResult("다시 시도해주세요", 503));
     }
 
+    @ApiOperation(value = "아이디/패스워드 찾기")
     @PostMapping("/useridsearch")
     ResponseEntity<?> userIdSearch(@RequestBody User user)throws Exception {
         System.out.println("userinfo: "+userServiceNew.findByNameAndPhoneNumber(user.getName(), user.getPhoneNumber()));
@@ -72,6 +79,7 @@ public class UserControllerNew {
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "휴면 계정 활성화")
     @PostMapping("/useractivation")
     ResponseEntity<?> userActiovation(@RequestBody User user)throws Exception {
         Map<String, String> map = new HashMap<>();
@@ -84,7 +92,7 @@ public class UserControllerNew {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new JsonResult("잠시후 다시 시도해 주세요.", 500));
     }
 
-
+    @ApiOperation(value = "패스워드 변경")
     @PostMapping("/passwordupdate")
     ResponseEntity<?> userPasswordUpdate(@RequestBody User data) throws Exception {
 
@@ -96,6 +104,7 @@ public class UserControllerNew {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new JsonResult("잠시후 다시 시도해 주세요.", 500));
     }
 
+    @ApiOperation(value = "비회원 로그인")
     @GetMapping("/unknownLogin")
     public ResponseEntity<?> unknownLogin(){
             User user = userServiceNew.saveUnknownUser();
