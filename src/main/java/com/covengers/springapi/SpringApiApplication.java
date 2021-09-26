7694bcead64b893d836539d93d0b7f3c07005c15
dntjd7701 <dntjd7701@gmail.com>
2021-09-26 17:51:55 +0900

    #Dr.[MERGE]:merge

--- src/main/java/com/covengers/springapi/SpringApiApplication.java
+++ src/main/java/com/covengers/springapi/SpringApiApplication.java
@@ -22,14 +22,17 @@ public class SpringApiApplication {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
+//
 //     Testing 을 위한 코드
 //     이 코드는 main 이 실행된 이후에 실행된다.
+
+
 //    @Bean
 //    CommandLineRunner run(UserServiceNew userServiceNew){
 //        Date date = new Date();
 //        return args -> {
 //            userServiceNew.saveUser(new User(null, "unknown", "unknown", "1111", "unknown", false, "http://simpleicon.com/wp-content/uploads/account.png", "http://simpleicon.com/wp-content/uploads/account.png", "ROLE_USER", "token", "unknown", date, date, "unknown"));
+//            userServiceNew.saveUser(new User(null, "Admin", "Admin", "covengers", "Admin", false, "http://simpleicon.com/wp-content/uploads/account.png", "http://simpleicon.com/wp-content/uploads/account.png", "ROLE_ADMIN", "token", "unknown", date, date, "Admin"));
 //            userServiceNew.saveUser(new User(null, "MirabelleTow@gmail.com", "Mirabelle Tow", "1111", "Mirabelle Tow", false, "http://simpleicon.com/wp-content/uploads/account.png", "http://simpleicon.com/wp-content/uploads/account.png", "ROLE_USER", "token", "01057289028", date, date, "covengers"));
 //            userServiceNew.saveUser(new User(null, "TownsendSeary@gmail.com", "Townsend Seary", "1111", "Townsend Seary", false, "http://simpleicon.com/wp-content/uploads/account.png", "http://simpleicon.com/wp-content/uploads/account.png", "ROLE_USER", "token", "01074980731", date, date, "covengers"));
 //            userServiceNew.saveUser(new User(null, "elsa@gmail.com", "elsa", "1111", "elsa", false, "http://simpleicon.com/wp-content/uploads/account.png", "http://simpleicon.com/wp-content/uploads/account.png", "ROLE_USER", "token", "01059284727", date, date, "covengers"));
